178a93c8169fe970cd5236667a393bd74c165cd2
kyfuvs <kyfuvs@gmail.com>
2024-01-07 12:17:53 -0500

    after connect git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      Vercel Deploy after modify 
+      Vercel Deploy after modify - after connect to git 
     </div>
   )
 }
