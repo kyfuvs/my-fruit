286000efc7e610f2d9c048df7f1b2f8ace3d1ef2
kyfuvs <kyfuvs@gmail.com>
2024-01-07 18:44:05 -0500

    after add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { PrismaClient } from '@prisma/client';
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const products = await prisma.product.findMany({
+   
+  });
+
   return (
     <div>
-      Vercel Deploy after modify - after connect to git 
+      <h1>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} 
+
+            <Image className='rounded-md'
+            src={`/${product.image}`}
+            width={30}
+            height={30}
+            alt='Image'
+           />
+
+            - $HKD {product.price} 
+            - {product.description} 
+            {/* - {product.detail.length > 0 ? product.detail[0].origin : 'No origin available'}
+            - {product.detail.length > 0 ? product.detail[0].year : 'No year available'} */}
+
+            {/* Add other properties you want to display */}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
